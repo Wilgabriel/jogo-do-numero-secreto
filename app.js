09d9bcaa96fcb29974e2b79dabeb4a3f90aad99e
Wilgabriel <wilgabimota@gmail.com>
2024-05-05 10:48:41 -0300

    mudando os nomes

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Braziliuan Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('h1', 'Jogo do número secretinho');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
